73fe9b1991b8a59cf2c3bcc3f82c11922027c0bf
uzadow <uzadow@libavg.de>
2006-05-09 20:17:05 +0000

    Added website.
    svn path=/trunk/www/; revision=106

---
+++ site/mouseovertext.js
@@ -0,0 +1,9 @@
+	function domouseover(alttext) 
+	 { 
+         document.getElementById('sc_mo').innerHTML=alttext;
+     }
+	 
+	 function domouseout()
+	 {
+         document.getElementById('sc_mo').innerHTML="";
+	 }
