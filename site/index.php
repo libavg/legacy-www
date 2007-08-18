b59ea18bae079c370b1a69b1d41313ee9c2701f3
uzadow <uzadow@libavg.de>
2007-08-18 07:48:51 +0000

    svn path=/trunk/www/; revision=2231

--- site/index.php
+++ site/index.php
@@ -31,6 +31,11 @@
   </div>
   
 </div>
-
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-2440175-1";
+urchinTracker();
+</script>
 </body>
 </html>
