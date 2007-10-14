3163cede5d295cedf052ecc7b261a6ae4dc14fc3
uzadow <uzadow@libavg.de>
2007-10-14 21:49:06 +0000

    Updated features.
    svn path=/trunk/www/; revision=2344

--- site/features.php
+++ site/features.php
@@ -19,7 +19,7 @@
   <div id="content" width="100"><h1>Features</h1>
     
 <p> 
-libavg includes a hardware-accelerated display mixer and screen layout engine and integrates high-performance video playback. It supports paragraph-level text layout and rendering as well as cameras. Nearly all parameters can be changed at runtime using python scripts. Input is supported using mouse, keyboard and touchscreens. Among others, libavg projects have used motors, coin rejectors and relays attached to parallel and serial ports, worked with usb bluetooth dongles and integrated printing.</p>
+libavg includes a hardware-accelerated display mixer and screen layout engine and integrates high-performance video playback. It supports paragraph-level text layout and rendering as well as cameras. Nearly all parameters can be changed at runtime using python scripts. Input is supported using mouse and keyboard as well as standard touchscreens and multitouch surfaces. Among others, libavg projects have used motors, coin rejectors and relays attached to parallel and serial ports, worked with usb bluetooth dongles and integrated printing.</p>
 
 <h2 class="news">Screen Layout Engine</h2> 
 
@@ -43,9 +43,10 @@ libavg includes a hardware-accelerated display mixer and screen layout engine an
 <p>libavg video support means:</p>
 <ul>
 <li>Playback pause and restart, playback at user-determined framerates</li>
-<li>Complete display engine integration, including cropping, scaling, rotation and transparency</li>
-<li>Very high performance: An Athlon 64 2.0 GHz machine can display 10 352x240 videos at once at 60 frames per second.</li>
-<li>Seeks at interactive rates: The same computer displays every third frame of a 1536x1024 video running backwards, also at 60 frames per second.</li>
+<li>Complete display engine integration, including cropping, scaling, rotation and transparency.</li>
+<li>Videos with an alpha channel are supported.</li>
+<li>Very high performance: A Core 2 Duo system can play back two full HD videos at once with the right setup. That's 2x1920x1080 pixels at 25 frames a second.</li>
+<li>Seeks at interactive rates: Essentially, libavg allows random access to the frames of full HD videos without delays.</li>
 </ul>
 
 
@@ -77,10 +78,8 @@ libavg includes a hardware-accelerated display mixer and screen layout engine an
 <h2 class="news">Authoring</h2> 
 <p>All of python is available for scripting. libavg directly provides:</p>
 <ul>
-
-
 <li>Properties of display elements as python variables</li>
-<li>Function calls when input events happen</li>
+<li>Full-featured event handling system</li>
 <li>Timers (setTimeout, setInterval)</li>
 <li>Support for logging</li>
 <li>High-level profiling to find performance bottlenecks</li>
