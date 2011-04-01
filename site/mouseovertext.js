ec7e030655a7390c8e17a5ee2010e096310f6778
uzadow <uzadow@libavg.de>
2011-04-01 18:18:30 +0000

    Removed old website.
    svn path=/trunk/www/; revision=5768

--- site/mouseovertext.js
+++
@@ -1,9 +0,0 @@
-	function domouseover(alttext) 
-	 { 
-         document.getElementById('sc_mo').innerHTML=alttext;
-     }
-	 
-	 function domouseout()
-	 {
-         document.getElementById('sc_mo').innerHTML="";
-	 }
